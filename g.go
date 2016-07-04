5a50817482197f43d0bdcabe4b6a69eaddf1dddd
Eric Mendes Dantas <ericdantas0@gmail.com>
2016-07-04 08:02:50 -0300

    ;

--- g.go
+++ g.go
@@ -3,63 +3,41 @@ package main
 import (
 	"bytes"
 	"compress/gzip"
+	"encoding/base64"
+	"encoding/json"
 	"io/ioutil"
+	"strconv"
 )
 
+const (
+	am = 100000
+)
+
+type todo struct {
+	ID  int    `json:"id"`
+	Msg string `json:"msg"`
+}
+
 func main() {
+	var ts []todo
+
+	for i := 0; i < am; i++ {
+		ts = append(ts, todo{
+			ID:  i,
+			Msg: "message " + strconv.Itoa(i),
+		})
+	}
+
 	var b bytes.Buffer
 
-	w := gzip.NewWriter(&b)
-	w.Write([]byte(`
-        alkdjakj kjasd
-         sdj 
-         asdj a
-         dj askd akdj asj 
-         sdj asd jsdkj aksdj ksdj a
-         d asjk ajsdk asjd ajd asd çasd a
-         sd 
-         sdj sk as
-         dj skj s
-         d asjasj
-          sdjks jasdk sdçask 
-          asdk asdk lk
-           sdklsk askd laskd çakd 
-           asd asdk aldk as
-           ç çj!
-           alkdjakj kjasd
-         sdj 
-         asdj a
-         dj askd akdj asj 
-         sdj asd jsdkj aksdj ksdj a
-         d asjk ajsdk asjd ajd asd çasd a
-         sd 
-         sdj sk as
-         dj skj s
-         d asjasj
-          sdjks jasdk sdçask 
-          asdk asdk lk
-           sdklsk askd laskd çakd 
-           asd asdk aldk as
-           ç çj!
-           alkdjakj kjasd
-         sdj 
-         asdj a
-         dj askd akdj asj 
-         sdj asd jsdkj aksdj ksdj a
-         d asjk ajsdk asjd ajd asd çasd a
-         sd 
-         sdj sk as
-         dj skj s
-         d asjasj
-          sdjks jasdk sdçask 
-          asdk asdk lk
-           sdklsk askd laskd çakd 
-           asd asdk aldk as
-           ç çj!
-    `))
+	bTs, _ := json.Marshal(ts)
 
-	w.Close()
-	w.Flush()
-
+	gz := gzip.NewWriter(&b)
+	gz.Write(bTs)
+	gz.Close()
+	gz.Flush()
 	ioutil.WriteFile("g.gz", b.Bytes(), 0644)
+	ioutil.WriteFile("g.json", bTs, 0644)
+	ioutil.WriteFile("g.b64.txt", []byte(base64.StdEncoding.EncodeToString(bTs)), 0644)
+
 }
